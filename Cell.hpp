a672e535e30917c578d4e6673a4c4cde6e59a997
Neptune Magic Sauce <37757244+NeptuneMagicSauce@users.noreply.github.com>
2020-11-27 19:13:56 +0100

    developer comments

--- Cell.hpp
+++ Cell.hpp
@@ -8,22 +8,23 @@ namespace Minus
     public:
         using RevealCallback = std::function<void(Cell&)>;
         Cell(const QColor&);
-        virtual void mousePressEvent(QMouseEvent *e) override;
-        virtual void mouseReleaseEvent(QMouseEvent *e) override;
 
         void raise(bool);
         void setNeighbors(std::vector<Cell*>& neighbors);
 
         static void setRevealCallback(RevealCallback);
 
+    protected:
+        virtual void mousePressEvent(QMouseEvent *e) override;
+        virtual void mouseReleaseEvent(QMouseEvent *e) override;
     private:
         const QColor color, sunken_color;
         bool raised { false };
     public:
         bool mine { false };
         bool revealed { false };
-        std::vector<Cell*> neighbors;
         int neighbor_mines { 0 };
+        std::vector<Cell*> neighbors;
     };
 
 };
