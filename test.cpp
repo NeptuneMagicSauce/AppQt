cc927a8d198862abcfbcfdaeabdb1c61339eb0d0
Neptune Magic Sauce <37757244+NeptuneMagicSauce@users.noreply.github.com>
2020-12-02 15:34:26 +0100

    mem leak fix

--- test.cpp
+++ test.cpp
@@ -297,6 +297,7 @@ namespace Minus
             }
 
             // populate neighbors
+            neighbors.clear();
             for (int x=0; x<width; ++x)
             {
                 for (int y=0; y<height; ++y)
