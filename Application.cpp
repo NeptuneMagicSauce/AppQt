0c492b3182e269091b44f222f045668d9c1d3024
Neptune Magic Sauce <37757244+NeptuneMagicSauce@users.noreply.github.com>
2020-12-07 00:19:43 +0100

    debug window for triggering errors

--- Application.cpp
+++ Application.cpp
@@ -1,28 +1,66 @@
 #include "Application.hpp"
 
+#include <vector>
+
+#include <QMessageBox>
 #include <QMainWindow>
+#include <QVBoxLayout>
 #include <QAction>
+#include <QPushButton>
 #include <QDebug>
 
 class ApplicationImpl
 {
 public:
+    QMessageBox* debug_window;
     void installDebugActions(QWidget* w)
     {
         auto* a = new QAction;
         a->setShortcut(QKeySequence("Ctrl+Alt+F1"));
         a->setShortcutContext(Qt::ApplicationShortcut);
-        QObject::connect(a, &QAction::triggered, [] () {
-            qDebug() << "dereference nullptr";
-            qDebug() << *(QPoint*)nullptr;
+        QObject::connect(a, &QAction::triggered, [this] () {
+            debug_window->show();
         });
         w->addAction(a);
+    }
+    void installDebugWindow(void)
+    {
+        debug_window = new QMessageBox;
+        debug_window->hide();
+        debug_window->setWindowTitle("Debug");
+        debug_window->setStandardButtons(QMessageBox::Close);
+        debug_window->setEscapeButton(QMessageBox::Close);//nullptr);
+        auto install_button = [this] (
+            const QString& label,
+            std::function<void()> cb) {
+            auto* button = new QPushButton;
+            button->setText(label);
+            // layout->addWidget(button);
+            debug_window->addButton(button, QMessageBox::DestructiveRole);
+            QObject::connect(button, &QAbstractButton::released, [=](){
+                qDebug() << "Debug" << label;
+                cb();
+            });
+        };
+
+        install_button("nullptr", []() {
+            qDebug() << *(QPoint*)nullptr;
+        });
+        install_button("assert(false)", []() {
+            assert(false);
+        });
+        install_button("vector.at()", []() {
+            std::vector<int> v;
+            v.resize(0);
+            qDebug() << v.at(0);
+        });
     }
 } impl_app;
 
 Application::Application(int argc, char** argv) :
     QApplication(argc, argv)
 {
+    impl_app.installDebugWindow();
     cb.setSingleShot(false);
     cb.setInterval(100);
     connect(&cb, &QTimer::timeout, [this] () {
