644cf78de8a2aac43461de57d8c0cc76bb31e493
Romain Lacroix <romain.lacroix@inria.fr>
2020-12-08 14:49:53 +0100

    crash handler windows specific: do it with interface and implementor

--- CrashHandler.hpp
+++ CrashHandler.hpp
@@ -6,61 +6,31 @@
 
 class CrashHandler
 {
-private:
-#if _WIN64
-    struct Win64
-    {
-        static void attach(void);
-        static bool isDebuggerAttached(void);
-        static void breakDebugger(void);
-    };
-#endif
 public:
+    static void Attach(void);
+    static void BreakDebugger(void);
+protected:
+
     class StackInfo
     {
     public:
         QString address;
         QString function;
         QString location;
-        QString prettyPrint(bool has_horizontal_scroll=false, bool rich_text=false) const;
+        QString prettyPrint(
+            bool has_horizontal_scroll=false,
+            bool rich_text=false) const;
     };
     using Stack = QList<StackInfo>;
 
-    static QStringList addr2line(const std::vector<void*>& addr);
-    static Stack formatStack(const QStringList& stack);
-    static bool hasAlreadyCrashed(void);
+    QStringList addr2line(const std::vector<void*>& addr);
+    Stack parseStack(const QStringList& stack);
+    bool hasAlreadyCrashed(void);
 
-    static void showDialog(const std::string& error, const Stack& stack);
-    static void showTerminal(const std::string& error, const Stack& stack);
+    void showDialog(const std::string& error, const Stack& stack);
+    void showTerminal(const std::string& error, const Stack& stack);
 
-    // TODO interface and implementor
-    static void attach(void)
-    {
-#if _WIN64
-        Win64::attach();
-#else
-#warning "Crash Handler not implemented"
-#endif
-    }
-    static bool isDebuggerAttached(void)
-    {
-#if _WIN64
-        return Win64::isDebuggerAttached();
-#else
-#warning "isDebuggerAttached not implemented"
-        return false;
-#endif
-    }
-    static void breakDebugger(void)
-    {
-        if (isDebuggerAttached() == false)
-        {
-            return;
-        }
-#if _WIN64
-        Win64::breakDebugger();
-#else
-#warning "breakDebugger not implemented"
-#endif
-    }
+    virtual bool isDebuggerAttached(void) const = 0;
+    virtual void breakDebugger(bool force=false) const = 0;
+
 };
---
+++ CrashHandlerWindows.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "CrashHandler.hpp"
+
+class CrashHandlerWin64: public CrashHandler
+{
+public:
+    CrashHandlerWin64(void);
+protected:
+    virtual bool isDebuggerAttached(void) const override;
+    virtual void breakDebugger(bool force=false) const override;
+private:
+    friend class CrashHandlerWin64Impl;
+    void handle(void* exception);
+    QStringList walkStack(void* exception);
+};
