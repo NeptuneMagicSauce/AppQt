787349ccb9eb28f24e5bbc12349a7505dd2da16f
Neptune Magic Sauce <37757244+NeptuneMagicSauce@users.noreply.github.com>
2020-12-09 02:35:29 +0100

    catch exceptions in crash handler

--- Utils.hpp
+++ Utils.hpp
@@ -2,8 +2,6 @@
 
 #include <string>
 #include <sstream>
-#include <set>
-#include <cxxabi.h>
 #include <QColor>
 
 namespace Utils
@@ -21,23 +19,14 @@ namespace Utils
         return QColor(int(red * 255), int(green * 255), int(blue * 255));
     }
 
-    inline void assertSingleton(const std::type_info& type)
-    {
-        const std::string class_name = abi::__cxa_demangle(type.name(), 0, 0, 0);
-        static std::set<std::string> classes;
-        if (classes.count(class_name))
-        {
-            throw std::runtime_error("only one instance supported of " + class_name);
-        }
-        classes.insert(class_name);
-    }
-
     template<class T> std::string toHexa(const T &value)
     {
         std::ostringstream oss;
         oss << std::hex << value;
         return oss.str();
     }
+
+    void assertSingleton(const std::type_info& type);
 
     void doAssert(
         bool condition,
@@ -46,7 +35,14 @@ namespace Utils
         const std::string& file,
         int line,
         const std::string& function);
+
+    void panicException(
+        const std::exception& e,
+        const std::string& file,
+        int line,
+        const std::string& function);
 };
 
 #define Assert(check) Utils::doAssert((check), (#check), "", __FILE__, __LINE__, __PRETTY_FUNCTION__)
 #define AssertX(check, message) Utils::doAssert((check), (#check), (message), __FILE__, __LINE__, __PRETTY_FUNCTION__)
+#define PanicException(ex) Utils::panicException(ex, __FILE__, __LINE__, __PRETTY_FUNCTION__)
