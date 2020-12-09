3672f9df3d720c9c3f3cec396f562f7f05de9a7b
Romain Lacroix <romain.lacroix@inria.fr>
2020-12-09 18:15:11 +0100

    developer comments

--- CrashHandlerWindows.cpp
+++ CrashHandlerWindows.cpp
@@ -40,9 +40,6 @@ void CrashHandlerWin64::handle(void* exception_void) const
 
     // TODO hide console on start, show console on crash
 
-    // TODO ship gdb qt pretty printers and eval-load them ?
-    // TODO XOR load in place on my machines? if $HOME/.gdb/... ?
-
     auto ex_code = exception ? exception->ExceptionRecord->ExceptionCode : 0;
     QString error_message =
         Utils::exceptionCode(ex_code) + " " +
