90be9cf7140134b41e1dde6a841e8f56d1c9c0db
Romain Lacroix <romain.lacroix@inria.fr>
2020-12-11 15:18:24 +0100

    decouple input event handling from Frame logic in parent class FrameInputEvents

--- Frame.hpp
+++ Frame.hpp
@@ -1,43 +1,35 @@
 #pragma once
 
-#include "CellWidget.hpp"
-#include "CellState.hpp"
+#include "FrameInputEvents.hpp"
+#include "Layout.hpp"
+#include "BackgroundOperation.hpp"
 
 namespace Minus
 {
-    class Frame: public QWidget
+    class Frame: public FrameInputEvents
     {
-        Q_OBJECT
     public:
         Frame(
             const int& width,
             const int& height,
             int max_width,
             int max_height);
+
         void reset(void);
         void setMineData(const CellStates& data);
         void revealCell(Indices);
 
-        const int& width;
-        const int& height;
-
         virtual void resizeEvent(QResizeEvent *event) override;
 
-    signals:
-        void reveal(Indices);
-        void autoRevealNeighbors(Indices);
-        void setFlag(Indices, bool);
-
     protected:
-        virtual void mousePressEvent(QMouseEvent *e) override;
-        virtual void mouseReleaseEvent(QMouseEvent *e) override;
-        virtual void mouseMoveEvent(QMouseEvent *e) override;
-        virtual void keyPressEvent(QKeyEvent *event) override;
-        virtual void keyReleaseEvent(QKeyEvent *event) override;
-        virtual void leaveEvent(QEvent *event) override;
+        virtual CellWidget* itemAt(int x, int y) override;
 
     private:
-        CellWidget* widgetOfEvent(QMouseEvent* e);
+        // cost of QWidget::setVisible is heavy with many instances
+        Utils::BackgroundOperation set_visible_operation;
+        QList<Indices> set_visible_indices;
+        Layout layout;
+        int cached_width, cached_height;
     };
 
 };
