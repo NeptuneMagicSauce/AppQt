f69739a1aa7519359f1bf3c310fbb8fb92b299a9
NeptuneMagicSauce <hidden.garbage.not.my.email@email.com>
2020-12-16 16:20:08 +0100

    class declaration of HSVDialog in cpp because it is private

--- src/utils/ColorDialog.hpp
+++ src/utils/ColorDialog.hpp
@@ -19,35 +19,5 @@ namespace Utils
     private:
         QFrame feedback;
         QColor color;
-        class HSVDialog : public QFrame
-        {
-        public:
-            enum struct Type { Hue, SatVal };
-            using Callback = std::function<void(int, int)>;
-            HSVDialog(Type type, const QColor& color, HSVDialog* linked_dialog, Callback callback);
-        protected:
-            QSize sizeHint() const override;
-            void paintEvent(QPaintEvent*) override;
-            void resizeEvent(QResizeEvent*) override;
-            void mousePressEvent(QMouseEvent*) override;
-            void mouseMoveEvent(QMouseEvent*) override;
-        private:
-            const Type type;
-            const Callback callback;
-            const QColor& color;
-            HSVDialog* linked_dialog;
-            int pos;
-            QPixmap pix;
-
-            void callCallback(int, int);
-            void updatePixmap(void);
-
-            static int hue(int x, int w);
-            static int sat(int x, int w);
-            static int val(int x, int w);
-
-            int position(int hue) const;
-            int position(int sat, int val) const;
-        };
     };
 }
